9612b13bcd9f4780b067f8416afe004343033db5
Tim Cherne <timcherne@gmail.com>
2020-05-19 14:50:12 -0500

    Error Handling
    only accept numbers for math problems
    divide by zero catch

--- educate.py
+++ educate.py
@@ -12,7 +12,7 @@ def get_arg_parser():
   parser.add_argument('-c', '--child', help='childs name', required=True)
   parser.add_argument('-m', '--min', help='min value for problems', default=0)
   parser.add_argument('-x', '--max', help='max value for problems', default=30)
-  parser.add_argument('-k', '--kind', help='add/sub/mult/div/spell', nargs='+', required=True)
+  parser.add_argument('-k', '--kind', help='add/sub/mult/div', nargs='+', required=True)
   parser.add_argument('-n', '--number_of_problems', help='number of problems', default=10)
   parser.add_argument('-r', '--results_directory', help='path to the results folder', required=True)
   return parser
@@ -28,8 +28,8 @@ def write_log(args, line):
 
 def generate_problem(args):
   kind = random.choice(args.kind)
-  start = args.min
-  stop = args.max
+  start = int(args.min)
+  stop = int(args.max)
   if kind == 'add':
     addition(args, random.randint(start, stop), random.randint(start, stop))
   elif kind == 'sub':
@@ -37,80 +37,106 @@ def generate_problem(args):
   elif kind == 'mult':
     multiplication(args, random.randint(start, stop), random.randint(start, stop))
   elif kind == 'div':
-    division(args, random.randint(start, stop), random.randint(start, stop))
-  elif kind == 'spell':
-    spelling()
+    division(args, random.randint(max(1, start), max(1, stop)), random.randint(max(1, start), max(1, stop)))
 
 def addition(args, num1, num2):
   correct = None
   prompt = ' '.join([str(num1), '+', str(num2), '= '])
-  reply = int(input(prompt))
-  answer = num1 + num2
-  if reply == answer:
-    print('Correct!')
-    correct = True
-  else:
-    print('Incorrect:')
-    print('\tThe correct answer is', prompt, answer)
-    correct = False
-  log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
-  write_log(args, log_entry)
-  print('\n')
+  while True:
+    reply = input(prompt)
+    if not reply.isdigit():
+      print('Sorry, ' + str(reply) + ' is not a number.' )
+      continue
+    else:
+      reply = int(reply)
+      answer = num1 + num2
+      if reply == answer:
+        print('Correct!')
+        correct = True
+      else:
+        print('Incorrect:')
+        print('\tThe correct answer is', prompt, answer)
+        correct = False
+      log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
+      write_log(args, log_entry)
+      print('\n')
+      #needed to exit the valid loop
+      break
 
 def subtraction(args, num1, num2):
   correct = None
   prompt = ' '.join([str(max(num1, num2)), '-', str(min(num1,num2)), '= '])
-  reply = int(input(prompt))
-  answer = max(num1, num2) - min(num1, num2)
-  if reply == answer:
-    print('Correct!')
-    correct = True
-  else:
-    print('Incorrect:')
-    print('\tThe correct answer is', prompt, answer)
-    correct = False
-  log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
-  write_log(args, log_entry)
-  print('\n')
+  while True:
+    reply = input(prompt)
+    if not reply.isdigit():
+      print('Sorry, ' + str(reply) + ' is not a number.' )
+      continue
+    else:
+      reply = int(reply)
+      answer = max(num1, num2) - min(num1, num2)
+      if reply == answer:
+        print('Correct!')
+        correct = True
+      else:
+        print('Incorrect:')
+        print('\tThe correct answer is', prompt, answer)
+        correct = False
+      log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
+      write_log(args, log_entry)
+      print('\n')
+      #needed to exit the valid loop
+      break
 
 def multiplication(args, num1, num2):
   correct = None
   prompt = ' '.join([str(num1), '*', str(num2), '= '])
-  reply = int(input(prompt))
-  answer = num1 * num2
-  if reply == answer:
-    print('Correct!')
-    correct = True
-  else:
-    print('Incorrect:')
-    print('\tThe correct answer is', prompt, answer)
-    correct = False
-  log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
-  write_log(args, log_entry)
-  print('\n')
+  while True:
+    reply = input(prompt)
+    if not reply.isdigit():
+      print('Sorry, ' + str(reply) + ' is not a number.' )
+      continue
+    else:
+      reply = int(reply)
+      answer = num1 * num2
+      if reply == answer:
+        print('Correct!')
+        correct = True
+      else:
+        print('Incorrect:')
+        print('\tThe correct answer is', prompt, answer)
+        correct = False
+      log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
+      write_log(args, log_entry)
+      print('\n')
+      #needed to exit the valid loop
+      break
 
 def division(args, num1, num2):
   correct = None
   print('First give the answer, then the remainder')
   prompt = ' '.join([str(max(num1, num2)), '/', str(min(num1,num2)), '= '])
-  reply = int(input(prompt))
-  reply_remainder = int(input('remainder = '))
-  answer = int(max(num1, num2) / min(num1, num2))
-  answer_remainder = max(num1, num2)%min(num1,num2)
-  if reply == answer and reply_remainder == answer_remainder:
-    print('Correct!')
-    correct = True
-  else:
-    print('Incorrect:')
-    print('\tThe correct answer is', prompt, answer, ' remainder = ', answer_remainder)
-    correct = False
-  log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
-  write_log(args, log_entry)
-  print('\n')
-
-def spelling():
-  print('your spelling is horrible.')
-  print('\n')
+  while True:
+    reply = input(prompt)
+    if not reply.isdigit():
+      print('Sorry, ' + str(reply) + ' is not a number.' )
+      continue
+    else:
+      reply = int(reply)
+      reply_remainder = int(input('remainder = '))
+      answer = int(max(num1, num2) / min(num1, num2))
+      answer_remainder = max(num1, num2)%min(num1,num2)
+      if reply == answer and reply_remainder == answer_remainder:
+        print('Correct!')
+        correct = True
+      else:
+        print('Incorrect:')
+        print('\tThe correct answer is', prompt, answer, ' remainder = ', answer_remainder)
+        correct = False
+      log_entry = ','.join([prompt, str(reply), str(answer), str(correct)])
+      write_log(args, log_entry)
+      print('\n')
+      #needed to exit the valid loop
+      break
 
 def main(args):
   print(args)
